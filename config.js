a2b2ec91f92ffa176ade0a411cfceeca1fa7d9e5
MINGli6669 <114544868+MINGli6669@users.noreply.github.com>
2024-02-25 08:10:42 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const config = {
   mode: "in", // 签到:in,签退:out
   accounts: [
     {
-      username: "", //用户名
-      password: "", //密码
+      username: "18764639145", //用户名
+      password: "mingli666", //密码
       openId: "", //微信小程序抓包openid(可选)
       unionId: "", //微信小程序抓包unionId(可选)
       sign: true, //是否自动签到
